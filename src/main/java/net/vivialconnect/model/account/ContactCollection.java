b0f749ab796e707b00e2be1b3682e0e3ee6cbb54
mgil <mgil@vivial.net>
2020-01-23 15:27:15 -0400

    Documented ContactCollection class

--- src/main/java/net/vivialconnect/model/account/ContactCollection.java
+++ src/main/java/net/vivialconnect/model/account/ContactCollection.java
@@ -3,12 +3,21 @@ package net.vivialconnect.model.account;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * This is class is a container for a collection of Contact.
+ * This is used during API's response JSON deserialization.
+ */
 public class ContactCollection{
-	
+
+    /**
+     * List of Contacts of an account
+     */
     @JsonProperty
     private List<Contact> contacts;
 
-
+    /**
+     * @return List of contacts
+     */
     public List<Contact> getContacts(){
         return contacts;
     }
