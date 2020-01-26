df7ff22238d79a0eabfc4ce763781bbd721927a6
mgil <mgil@vivial.net>
2020-01-25 20:48:44 -0400

    Documented TagCollection

--- src/main/java/net/vivialconnect/model/number/TagCollection.java
+++ src/main/java/net/vivialconnect/model/number/TagCollection.java
@@ -1,24 +1,46 @@
 package net.vivialconnect.model.number;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Map;
 
+/**
+ * Wrapper class used for map the result of a collection of tags returned by the API.
+ */
 public class TagCollection {
 
+    /**
+     * Collection of tags.
+     */
     @JsonProperty("tags")
     private Map<String, String> tags;
 
     public TagCollection() {
     }
 
+    /**
+     * Convenient constructor for create and set a collection of tags.
+     *
+     * @param tags collection of tags value
+     */
     public TagCollection(Map<String, String> tags) {
         this.tags = tags;
     }
 
+    /**
+     * Collection of tags
+     *
+     * @return collection of tags
+     */
     public Map<String, String> getTags() {
         return tags;
     }
 
+    /**
+     * Set the tags collection value
+     *
+     * @param tags tags collection value
+     */
     public void setTags(Map<String, String> tags) {
         this.tags = tags;
     }
