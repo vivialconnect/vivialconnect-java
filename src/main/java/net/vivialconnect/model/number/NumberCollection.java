01e54e5bd0472d756f620e7e88b61e99cf201809
mgil <mgil@vivial.net>
2020-01-25 20:37:44 -0400

    Documented NumberCollection class

--- src/main/java/net/vivialconnect/model/number/NumberCollection.java
+++ src/main/java/net/vivialconnect/model/number/NumberCollection.java
@@ -4,12 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Wrapper class that contains a collection of phone numbers.
+ *
+ * This class is used for map the results of:
+ *
+ * <ul>
+ *     <li>API Available numbers call</li>
+ *     <li>API Associated numbers call</li>
+ * </ul>
+ *
+ */
 public class NumberCollection {
-	
+
+    /**
+     * Collection of phone number entities
+     * @see Number
+     * @see AssociatedNumber
+     * @see AvailableNumber
+     */
     @JsonProperty("phone_numbers")
     private List<Number> numbers;
 
-
+    /**
+     * @return a collection of available numbers
+     */
     public List<AvailableNumber> getAvailableNumbers() {
         List<AvailableNumber> availableNumbers = new ArrayList<AvailableNumber>(numbers.size());
         for (Number number : numbers){
@@ -19,7 +38,9 @@ public class NumberCollection {
         return availableNumbers;
     }
 
-
+    /**
+     * @return a collection of associated numbers in the account
+     */
     public List<AssociatedNumber> getAssociatedNumbers() {
         List<AssociatedNumber> associatedNumbers = new ArrayList<AssociatedNumber>(numbers.size());
         for (Number number : numbers){
@@ -29,7 +50,10 @@ public class NumberCollection {
         return associatedNumbers;
     }
 
-
+    /**
+     * Set a collection of numbers
+     * @param numbers collection of numbers value
+     */
     public void setNumbers(List<Number> numbers) {
         this.numbers = numbers;
     }
