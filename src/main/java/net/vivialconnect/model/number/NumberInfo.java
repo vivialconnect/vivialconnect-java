ba03c8d0a8ae42b7f8682c7c0599dc7d41c794d5
mgil <mgil@vivial.net>
2020-01-25 20:44:29 -0400

    Documented NumberInfo class

--- src/main/java/net/vivialconnect/model/number/NumberInfo.java
+++ src/main/java/net/vivialconnect/model/number/NumberInfo.java
@@ -1,6 +1,7 @@
 package net.vivialconnect.model.number;
 
 import java.util.Date;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import net.vivialconnect.model.VivialConnectResource;
@@ -10,59 +11,101 @@ import net.vivialconnect.model.VivialConnectResource;
  * <p>
  * This information can be useful for text messaging campaigns that target specific user experiences based on the
  * user’s phone number information.
- * */
+ */
 @JsonRootName(value = "number_info")
-public class NumberInfo extends VivialConnectResource{
-	
+public class NumberInfo extends VivialConnectResource {
+
     private static final long serialVersionUID = 8828026309873208187L;
 
-    /** Details about the phone carrier providing service to the phone number */
+    /**
+     * Details about the phone carrier providing service to the phone number
+     */
     @JsonProperty
     private Carrier carrier;
 
-    /** Details about the device type used by the phone number */
+    /**
+     * Details about the device type used by the phone number
+     */
     @JsonProperty
     private Device device;
 
-    /** Phone number for which you want device and carrier information */
+    /**
+     * Phone number for which you want device and carrier information
+     */
     @JsonProperty("phone_number")
     private String phoneNumber;
 
-    /** Creation date (UTC) of the number info in ISO 8601 format */
+    /**
+     * Creation date (UTC) of the number info in ISO 8601 format
+     */
     @JsonProperty("date_created")
     private Date dateCreated;
 
-    /** Last modification date (UTC) of number info in ISO 8601 format */
+    /**
+     * Last modification date (UTC) of number info in ISO 8601 format
+     */
     @JsonProperty("date_modified")
     private Date dateModified;
 
-
-    public Carrier getCarrier(){
+    /**
+     * Details about the phone carrier providing service to the phone number.
+     *
+     * @return carrier name value
+     */
+    public Carrier getCarrier() {
         return carrier;
     }
 
-
-    public void setCarrier(Carrier carrier){
+    /**
+     * Set the carrier name value.
+     * <strong>Note:</strong> Setting this value does not have any effect in the  API
+     *
+     * @param carrier carrier name value
+     */
+    public void setCarrier(Carrier carrier) {
         this.carrier = carrier;
     }
 
-
-    public Device getDevice(){
+    /**
+     * Details about the device type used by the phone number.
+     * <p>
+     * Results include:
+     * <p>
+     * - model - Model name of the device.
+     * - error - Information about errors in device lookup. For example, number is not mobile or carrier doesn’t support device lookup.
+     *
+     * @return Device object with info about the device to which a number belongs
+     */
+    public Device getDevice() {
         return device;
     }
 
-
-    public void setDevice(Device device){
+    /**
+     * Set a Device object.
+     * <strong>Note:</strong> Setting this value does not have any effect in the  API
+     *
+     * @param device a Device object
+     */
+    public void setDevice(Device device) {
         this.device = device;
     }
 
-
-    public String getPhoneNumber(){
+    /**
+     * Phone number for which you want device and carrier information.
+     *
+     * @return a phone number value
+     */
+    public String getPhoneNumber() {
         return phoneNumber;
     }
 
-
-    public void setPhoneNumber(String phoneNumber){
+    /**
+     * Set the phone number value
+     * <strong>Note:</strong> Setting this value does not have any effect in the  API
+     *
+     * @param phoneNumber phone number value
+     */
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 }
