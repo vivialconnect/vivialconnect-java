74536ff0e84605154d59ee4f331ef45cebd5217e
mgil <mgil@vivial.net>
2020-01-27 13:12:10 -0400

    Documented ConnectorWithCallbacks class

--- src/main/java/net/vivialconnect/model/connector/ConnectorWithCallbacks.java
+++ src/main/java/net/vivialconnect/model/connector/ConnectorWithCallbacks.java
@@ -3,10 +3,22 @@ package net.vivialconnect.model.connector;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * This interface defines the properties and methods for a connector that contains callbacks
+ */
 public interface ConnectorWithCallbacks {
-	
+
+    /**
+     * Must return the last modification date of the connector
+     *
+     * @return connector's last modification date
+     */
     Date getDateModified();
-	
-	
+
+    /**
+     * Must return a list of callbacks
+     *
+     * @return connector's callbacks list
+     */
     List<Callback> getCallbacks();
 }
