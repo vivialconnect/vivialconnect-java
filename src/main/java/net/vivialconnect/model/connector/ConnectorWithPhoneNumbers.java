e1c0d4b24bb4dd96cf9d03dd17c7a5eb8f38ea22
mgil <mgil@vivial.net>
2020-01-27 13:29:19 -0400

    Documented ConnectorWithPhoneNumbers class

--- src/main/java/net/vivialconnect/model/connector/ConnectorWithPhoneNumbers.java
+++ src/main/java/net/vivialconnect/model/connector/ConnectorWithPhoneNumbers.java
@@ -5,22 +5,66 @@ import net.vivialconnect.model.error.VivialConnectException;
 import java.util.Date;
 import java.util.List;
 
-public interface ConnectorWithPhoneNumbers{
-
+/**
+ * This interface defines the basic properties and methods for a connector that contains a list phone numbers
+ */
+public interface ConnectorWithPhoneNumbers {
+    /**
+     * Must return the last modification date of the connector
+     *
+     * @return last modification date of the connector
+     */
     Date getDateModified();
 
-	int getPhoneNumbersCount();
+    /**
+     * Must return count of phone numbers associated to the connector
+     *
+     * @return count of numbers associated to the connector
+     */
+    int getPhoneNumbersCount();
 
-	int nextPage() throws VivialConnectException;
+    /**
+     * Must paginate to the next page and return its page number
+     *
+     * @return next page value
+     * @throws VivialConnectException if an error occurs at API-level
+     */
+    int nextPage() throws VivialConnectException;
 
-	int previousPage() throws VivialConnectException;
+    /**
+     * Must paginate to the previous page and return its page number
+     *
+     * @return previous page value
+     * @throws VivialConnectException if an error occurs at API-level
+     */
+    int previousPage() throws VivialConnectException;
 
-	int getNextPage();
+    /**
+     * Must return the next page value
+     *
+     * @return next page value
+     */
+    int getNextPage();
 
-	int getPreviousPage();
+    /**
+     * Must return the previous page value
+     *
+     * @return previous page value
+     */
+    int getPreviousPage();
 
-	int getPages();
+    /**
+     * Must return count of pages
+     *
+     * @return count of pages value
+     */
+    int getPages();
 
-	List<PhoneNumber> getPhoneNumbers();
+    /**
+     * Must return a list of phone numbers associated to a connector
+     *
+     * @return list of phone numbers associated to a connector
+     */
+    List<PhoneNumber> getPhoneNumbers();
 
 }
