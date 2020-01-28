a2413c230b480854be3146a32a1dfc5e118acc30
mgil <mgil@vivial.net>
2020-01-28 09:21:46 -0400

    Documented UserCollection class

--- src/main/java/net/vivialconnect/model/user/UserCollection.java
+++ src/main/java/net/vivialconnect/model/user/UserCollection.java
@@ -4,18 +4,35 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class UserCollection{
+/**
+ * Wrapper class for a list of users returned by the API
+ *
+ * @see User
+ */
+public class UserCollection {
 
+    /**
+     * List of users
+     */
     @JsonProperty
     private List<User> users;
 
-
-    public List<User> getUsers(){
+    /**
+     * Returns a list of users
+     *
+     * @return list of users
+     */
+    public List<User> getUsers() {
         return users;
     }
 
 
-    public void setUsers(List<User> users){
+    /**
+     * Set a list of users
+     *
+     * @param users list of users
+     */
+    public void setUsers(List<User> users) {
         this.users = users;
     }
 }
